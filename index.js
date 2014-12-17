3549ea4ce767400de9c5f099d8d6afa2194e345e
treelite <c.xinle@gmail.com>
2014-12-17 14:05:23 +0800

    reconstruction, handle json and template files

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ function add(type, handler) {
     handlers[type] = handler;
 }
 
-add('route', require('./lib/route'));
-add('tpl', require('./lib/tpl'));
+add('json/route', require('./lib/json/route'));
+add('json/tpl', require('./lib/json/tpl'));
+add('template', require('./lib/template'));
 
-module.exports = function (type, file) {
+module.exports = function (type, data, options) {
     var handler = handlers[type];
     if (!handler) {
         return;
     }
 
-    return handler(file);
+    return handler(data, options);
 };
 
 module.exports.edpWebserver = require('./lib/adapter/edp-webserver');
+module.exports.edpBuild = require('./lib/adapter/edp-build');
